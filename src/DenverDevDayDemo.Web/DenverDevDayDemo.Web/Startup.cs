d68048d236e6908cd920a013c1a7b725f9104613
Shawn Kendrot <skendrot@hotmail.com>
2016-06-24 09:04:08 -0600

    First commit

---
+++ src/DenverDevDayDemo.Web/DenverDevDayDemo.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DenverDevDayDemo.Web.Startup))]
+namespace DenverDevDayDemo.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
